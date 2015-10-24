223e60cfb34aa818ebc9f1be9eb711c0f0944d95
David McConnell <david.mcconnell@tufts.edu>
2015-10-24 07:45:32 -0400

    yolo

--- public/scriptsheet.js
+++ public/scriptsheet.js
@@ -19,9 +19,12 @@ var map;
 var lat = 0;
 var lng = 0;
 var school = "Harvard University";
+var address = "27 Packard Ave Medford, MA 02144"
 var url_school = encodeURIComponent(school);
+var url_address = encodeURIComponent(address);
 
 var marker;
+var house_marker;
 
 var map_request;
 var form_request;
@@ -47,13 +50,13 @@ function displaySignUp() {
 	[].slice.call(elems).forEach(function (elem) {
 		$(elem).fadeOut("slow");
 	});
-	elems = document.getElementsByClassName("loginform");
+	elems = document.getElementsByClassName("loginbg");
 	[].slice.call(elems).forEach(function (elem) {
 		$(elem).fadeIn("slow");
-		elem.innerHTML = '<section class = "login-page"><h1>Welcome</h1><form><table><tr>' + 
-		'<td><label>Email:</label></td><td><input name="user[email]" type="text" value=""></td></tr><tr>' +
-	    '<td><label>Password:</label></td><td><input name="user[password]" type="password" value=""></td>' + 
-	    '</tr></table><input type="submit" value="Login" class="submit"/></form></section>';
+		elem.innerHTML = '<img src="./poly_design/login_page/loginbackground.png"/><div class=loginform><section class = "login-page"><form><table><tr>' + 
+		'<td></td><td><input name="user[email]" type="text" value=""></td></tr><tr>' +
+	    '<td></td><td><input name="user[password]" type="password" value=""></td>' + 
+	    '</tr></table><input type="submit" value="" class="submit"/></form></section></div>';
 	});
 	
 
@@ -91,27 +94,10 @@ function displayLogin() {
 	// });
 }
 
-function run_home() {
-	map = new google.maps.Map(document.getElementById("map_image"), map_options);
-
-	//if allowed by the user, get the lat and lng and call make_request()
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			lat = position.coords.latitude;
-			lng = position.coords.longitude;
-			draw_map();
-		});
-	}
-	//otherwise print an error message on the page
-	else {
-		elem = document.getElementById("rendered_map");
-		elem.innerHTML = "Enable location services to view your location.";
-	}
-}
-
 function scroll() {
 	autoScrollTo("map_image");
 }
+
 function autoScrollTo(el) {
 	  var currentY = window.pageYOffset;
 	  var targetY = document.getElementById(el).offsetTop;
@@ -158,19 +144,37 @@ function send_form() {
 	}
 }
 
+function run_home() {
+	map = new google.maps.Map(document.getElementById("map_image"), map_options);
+
+	//if allowed by the user, get the lat and lng and call make_request()
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			lat = position.coords.latitude;
+			lng = position.coords.longitude;
+			draw_map();
+			run_other();
+			});
+		}
+
+	//otherwise print an error message on the page
+	else {
+		elem = document.getElementById("rendered_map");
+		elem.innerHTML = "Enable location services to view your location.";
+	}
+}
 
 function run_other() {
-	map = new google.maps.Map(document.getElementById("gmap"), map_options);
+	//map = new google.maps.Map(document.getElementById("gmap"), map_options);
 
 	map_request = new XMLHttpRequest();
-	map_request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url_school + '&key=AIzaSyDgx4J49sV4eFMRM-b0HxqM7Cq_0c8Nu48', true);
+	map_request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url_address + '&key=AIzaSyDgx4J49sV4eFMRM-b0HxqM7Cq_0c8Nu48', true);
 	map_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	map_request.send();
 	map_request.onreadystatechange = function() {
 		if (map_request.readyState == 4 && map_request.status == 200) {
 			response = map_request.responseText;
 			parsed = JSON.parse(response);
-			console.log(parsed);
 			lat = parsed["results"][0]["geometry"]["location"]["lat"];
 			lng = parsed["results"][0]["geometry"]["location"]["lng"];
 			draw_house();
@@ -180,22 +184,22 @@ function run_other() {
 };
 
 function draw_map() {
-	marker = new google.maps.Marker({
+	house_marker = new google.maps.Marker({
 		position: new google.maps.LatLng(lat, lng),
 		title: "<p>You</p>"
 	});
 
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
+	google.maps.event.addListener(house_marker, 'click', function() {
+		infowindow.setContent(house_marker.title);
+		infowindow.open(map, house_marker);
 	});
 
 	google.maps.event.addListenerOnce(map, 'idle', function(){
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
+		infowindow.setContent(house_marker.title);
+		infowindow.open(map, house_marker);
 	});
 
-	marker.setMap(map);
+	house_marker.setMap(map);
 
 	map_center = new google.maps.LatLng(lat, lng);
 
@@ -203,9 +207,11 @@ function draw_map() {
 }
 
 function draw_house() {
+	
 	marker = new google.maps.Marker({
 		position: new google.maps.LatLng(lat, lng),
-		title: "<p>You</p>"
+		icon: "./poly_design/pin.png",
+		title: "<p>A house</p>"
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
@@ -213,14 +219,5 @@ function draw_house() {
 		infowindow.open(map, marker);
 	});
 
-	google.maps.event.addListenerOnce(map, 'idle', function(){
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
-	});
-
 	marker.setMap(map);
-
-	map_center = new google.maps.LatLng(lat, lng);
-
-	map.panTo(map_center);
 }
