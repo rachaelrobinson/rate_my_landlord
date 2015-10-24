26171e1ed164e18ebc60afb8991871ad6a21ceed
David McConnell <david.mcconnell@tufts.edu>
2015-10-23 21:41:18 -0400

    stuff

---
+++ public/js/scriptsheet.js
@@ -0,0 +1,23 @@
+/*script haha wahoo*/
+
+//center of the map to be created
+var map_center = new google.maps.LatLng(42.4069, 71.1198);
+
+//options to be set when the map is created
+var map_options =  {
+				zoom: 12,
+				center: map_center,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+//window containing info for when a marker is clicked
+var infowindow = new google.maps.InfoWindow();
+
+//the map itself
+var map;
+
+function run() {
+	map = new google.maps.Map(document.getElementById("gmap"), map_options);
+
+	
+};
