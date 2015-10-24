d4326189a2ff585969e8ec5246696d775f344af0
David McConnell <david.mcconnell@tufts.edu>
2015-10-24 05:46:38 -0400

    login lol

--- public/scriptsheet.js
+++ public/scriptsheet.js
@@ -35,11 +35,25 @@ function init() {
 }
 
 function displaySignUp() {
-	new_elem = document.getElementById("login");
-	elems = document.getElementsByClassName("homesweethome", "howitworks");
+	elems = document.getElementsByClassName("homesweethome");
 	[].slice.call(elems).forEach(function (elem) {
 		$(elem).fadeOut("slow");
 	});
+	elems = document.getElementsByClassName("howitworks");
+	[].slice.call(elems).forEach(function (elem) {
+		$(elem).fadeOut("slow");
+	});
+	elems = document.getElementsByClassName("loginform");
+	[].slice.call(elems).forEach(function (elem) {
+		$(elem).fadeIn("slow");
+		elem.innerHTML = '<section class = "login-page"><h1>Welcome</h1><form><table><tr>' + 
+		'<td><label>Email:</label></td><td><input name="user[email]" type="text" value=""></td></tr><tr>' +
+	    '<td><label>Password:</label></td><td><input name="user[password]" type="password" value=""></td>' + 
+	    '</tr></table><input type="submit" value="Login" class="submit"/></form></section>';
+	});
+	
+
+
 }
 
 function displayLogin() {
