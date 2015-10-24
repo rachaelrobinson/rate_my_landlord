954eced84ab76fde3e4b05c64abf11dfe2dcf192
David McConnell <david.mcconnell@tufts.edu>
2015-10-24 01:47:32 -0400

    updates

--- public/scriptsheet.js
+++ public/scriptsheet.js
@@ -23,7 +23,8 @@ var url_school = encodeURIComponent(school);
 
 var marker;
 
-var request;
+var map_request;
+var form_request;
 function init() {
 	elem = document.getElementsByClassName("page_top");
 	[].slice.call(elem).forEach(function (div) {
@@ -32,14 +33,16 @@ function init() {
 	if (window.location.pathname != "/") {
 		run();
 	}
-	request = new XMLHttpRequest();
-	request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url_school + '&key=AIzaSyDgx4J49sV4eFMRM-b0HxqM7Cq_0c8Nu48', true);
-	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	request.send();
-	request.onreadystatechange = function() {
-		console.log(request.readyState);
-		if (request.readyState == 4 && request.status == 200) {
-			response = request.responseText;
+}
+
+function send_form() {
+	form_request = new XMLHttpRequest();
+	form_request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url_school + '&key=AIzaSyDgx4J49sV4eFMRM-b0HxqM7Cq_0c8Nu48', true);
+	form_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	form_request.send();
+	form_request.onreadystatechange = function() {
+		if (form_request.readyState == 4 && form_request.status == 200) {
+			response = form_request.responseText;
 			parsed = JSON.parse(response);
 			console.log(parsed);
 			lat = parsed["results"][0]["geometry"]["location"]["lat"];
@@ -52,6 +55,21 @@ function init() {
 
 function run() {
 	map = new google.maps.Map(document.getElementById("gmap"), map_options);
+
+	map_request = new XMLHttpRequest();
+	map_request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url_school + '&key=AIzaSyDgx4J49sV4eFMRM-b0HxqM7Cq_0c8Nu48', true);
+	map_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	map_request.send();
+	map_request.onreadystatechange = function() {
+		if (map_request.readyState == 4 && map_request.status == 200) {
+			response = map_request.responseText;
+			parsed = JSON.parse(response);
+			console.log(parsed);
+			lat = parsed["results"][0]["geometry"]["location"]["lat"];
+			lng = parsed["results"][0]["geometry"]["location"]["lng"];
+			draw_map();
+		}
+	}
 
 };
 
