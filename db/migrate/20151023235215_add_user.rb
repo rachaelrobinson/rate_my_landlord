175fa988063abe62a5d439b82d01397d51ecbf74
Rachael Robinson <Rachael@resnet157-78.medford.tufts.edu>
2015-10-23 20:37:23 -0400

    new gemfile and gems using sql

---
+++ db/migrate/20151023235215_add_user.rb
@@ -0,0 +1,14 @@
+class AddUser < ActiveRecord::Migration
+  def change
+  	create_table :Student do |t|
+      t.string :email
+      t.string :password
+      t.string :university
+      t.integer :house_num
+      t.string :house_street
+      t.integer :house_zip
+      t.string :city
+      t.string :state
+    end
+  end
+end
